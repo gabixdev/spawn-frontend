6db2707f5aff32af59ac2af2cc59aacf336d67eb
Michał Frąckiewicz <hello@systemz.pl>
2016-06-04 15:47:49 +0200

    More complete access page

--- src/components/Access.vue
+++ src/components/Access.vue
@@ -1,5 +1,49 @@
 <template>
+
   <h2 class="text-huge">Dostęp</h2>
   <h3 class="text-big">Hasło</h3>
+  <p>
+    Hasło jest bardzo ważne, powinineś znać je tylko ty.
+    <br> Rekomendowane jest aby co jakiś czas je zmieniać.
+  </p>
+  <div class="row">
+    <div class="col-xs-12">
+      <label class="label">Obecne hasło</label>
+      <div class="input">
+        <input placeholder="Hasło przed zmianą" type="text">
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <label class="label">Nowe hasło</label>
+      <div class="input">
+        <input placeholder="Hasło po zmianie" type="text">
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <label class="label">Potwórz nowe hasło</label>
+      <div class="input">
+        <input placeholder="Nowe hasło, jeszcze raz" type="text">
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <button class="button button-green button-big block-mobile">Zmień</button>
+    </div>
+  </div>
   <h3 class="text-big">Klucz API</h3>
+  <p>
+    Klucz API umożliwa korzystanie z narzędzi dla programistów.
+    <br> Daje te same możliwości co hasło do konta więc nie udostępniaj go innym osobom!
+  </p>
+  <div class="row">
+    <div class="col-xs-12">
+      <button class="button button-green button-big block-mobile">Wyświetl klucz API</button>
+    </div>
+  </div>
+
 </template>
